13d0ab865b47ad032e2c8b2ea2ca5231aba4f720
Bill Collins <bilbocollins@gmail.com>
2015-09-11 16:46:51 +0100

    rm dist

--- dist/js/menu.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('a[href="#global-header-drawer-close"]').click(function() {
-    $('#global-header-drawers div.tab-pane.active').removeClass('active');
-    $('.navbar-global a.nav-link.active').removeClass('active');
-  });
-});
