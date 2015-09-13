195db0efb5bf9981ac75c783e84733447cd70c68
Bill Collins <bilbocollins@gmail.com>
2015-09-13 10:29:03 +0100

    Tidy up css

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('a[href="#global-header-drawer-close"]').click(function() {
     $('#global-header-drawers div.tab-pane.active').removeClass('active');
-    $('.navbar-global a.nav-link.active').removeClass('active');
+    $('.global-header a.nav-link.active').removeClass('active');
   });
   
   $('[data-toggle="offcanvas"]').click(function(e) {
