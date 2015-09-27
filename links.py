923217ee2e5b3d3cf403bc5f88a2a410711d8ee0
Bill Collins <bilbocollins@gmail.com>
2015-09-27 20:15:44 +0100

    footer links to py

--- links.py
+++ links.py
@@ -1,14 +1,14 @@
-__all__ = ('QUICKLINKS', 'STUDY_DRAWER', 'ABOUT_DRAWER')
+__all__ = ('QUICKLINKS', 'STUDY_DRAWER', 'ABOUT_DRAWER', 'FOOTER_LINKS')
 
 QUICKLINKS = (
-  ("http://www.cam.ac.uk/for-staff", "For staff"),
-  ("http://www.admin.cam.ac.uk/students/gateway", "For current students"),
-  ("http://www.alumni.cam.ac.uk/", "For alumni"),
-  ("http://www.cam.ac.uk/for-business", "For business"),
-  ("http://www.cam.ac.uk/colleges-and-departments", "Colleges & departments"),
-  ("http://www.cam.ac.uk/libraries-and-facilities", "Libraries & facilities"),
-  ("http://www.cam.ac.uk/museums-and-collections", "Museums & collections"),
-  ("http://www.cam.ac.uk/email-and-phone-search", "Email & phone search"),
+  ('http://www.cam.ac.uk/for-staff', 'For staff'),
+  ('http://www.admin.cam.ac.uk/students/gateway', 'For current students'),
+  ('http://www.alumni.cam.ac.uk/', 'For alumni'),
+  ('http://www.cam.ac.uk/for-business', 'For business'),
+  ('http://www.cam.ac.uk/colleges-and-departments', 'Colleges & departments'),
+  ('http://www.cam.ac.uk/libraries-and-facilities', 'Libraries & facilities'),
+  ('http://www.cam.ac.uk/museums-and-collections', 'Museums & collections'),
+  ('http://www.cam.ac.uk/email-and-phone-search', 'Email & phone search'),
 )
 
 STUDY_DRAWER = {
@@ -67,3 +67,49 @@ ABOUT_DRAWER = {
     ],
   ],
 }
+
+FOOTER_LINKS = [
+  {
+    'header': None,
+    'links': [
+      ('University A-Z', 'http://www.cam.ac.uk/university-a-z'),
+      ('Contact the University', 'http://www.cam.ac.uk/contact-the-university'),
+      ('Accessibility', 'http://www.cam.ac.uk/about-this-site/accessibility'),
+      ('Freedom of information', 'http://www.admin.cam.ac.uk/univ/information/foi/'),
+      ('Terms and conditions', 'http://www.cam.ac.uk/about-this-site/terms-and-conditions'),
+    ],
+  },
+  {
+    'header': ('Study at Cambridge', 'http://www.cam.ac.uk/study-at-cambridge'),
+    'links': [
+      ('Undergraduate', 'http://www.study.cam.ac.uk/undergraduate/'),
+      ('Graduate', 'http://www.admin.cam.ac.uk/students/gradadmissions/prospec/'),
+      ('International students', 'http://www.cam.ac.uk/about-the-university/international-cambridge/studying-at-cambridge'),
+      ('Continuing education', 'http://www.ice.cam.ac.uk/'),
+      ('Executive and professional education', 'http://www.admin.cam.ac.uk/offices/education/epe/'),
+      ('Courses in education', 'http://www.educ.cam.ac.uk/'),
+    ],
+  },
+  {
+    'header': ('About the University', 'http://www.cam.ac.uk/about-the-university'),
+    'links': [
+      ('How the University and Colleges work', 'http://www.cam.ac.uk/about-the-university/how-the-university-and-colleges-work'),
+      ('Visiting the University', 'http://www.cam.ac.uk/about-the-university/visiting-the-university'),
+      ('Map', 'http://map.cam.ac.uk/'),
+      ('News', 'http://www.cam.ac.uk/news'),
+      ('Events', 'http://www.admin.cam.ac.uk/whatson'),
+      ('Jobs', 'http://www.jobs.cam.ac.uk/'),
+      ('Giving to Cambridge', 'http://www.philanthropy.cam.ac.uk/'),
+    ],
+  },
+  {
+    'header': ('Research at Cambridge', 'http://www.cam.ac.uk/research'),
+    'links': [
+      ('News', 'http://www.cam.ac.uk/research/news'),
+      ('Features', 'http://www.cam.ac.uk/research/features'),
+      ('Discussion', 'http://www.cam.ac.uk/research/discussion'),
+      ('Spotlight on...', 'http://www.cam.ac.uk/research/spotlight-on'),
+      ('About research at Cambridge', 'http://www.cam.ac.uk/research/research-at-cambridge'),
+    ],
+  },
+]
