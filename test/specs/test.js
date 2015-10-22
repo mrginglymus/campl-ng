9239a385a2c4fcb45d7ab81be3a968ae82b8b135
Bill Collins <bilbocollins@gmail.com>
2015-10-22 07:23:15 +0100

    haha

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,34 +1,18 @@
 var webdriverio = require('webdriverio');
-var options = require('../wdio.conf').config;
-var client = webdriverio.remote(options).init();
-
 
 describe("this is my test suite", function() {
 
-  var client = {}
 
-  beforeEach(function(done) {
-    client = webdriverio.remote(options);
-    client.init(done);
-  });
 
-  it("this is my test spec", function() {
+  it("this is my test spec", function(done) {
     a = true;
     expect(a).toBe(true);
+    browser.url('/')
+      .getTitle(function(err, title) {
+        expect(title).toBe('CamPL-NG: Home');
+        done();
+      });
   });
 
-  afterEach(function(done) {
-    client.end(done);
-  });
+
 });
-
-
-client
-  .init()
-  .url('/')
-  .getTitle(function(err, title) {
-    console.log('hi');
-    console.log(err);
-    console.log(title);
-  })
-  .end();
