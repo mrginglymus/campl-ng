f0892671b42d47a2fab7e26fc11f93326ed56bad
Bill Collins <bill.collins@realvnc.com>
2017-12-08 18:00:09 +0000

    phewweeee

--- make.py
+++ make.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 
-import os
-import shutil
-import urllib
-from subprocess import call
 import argparse
+
 import simplejson as json
-from ordereddict import OrderedDict
 from jinja2 import FileSystemLoader, Environment
-import codecs
-
-
-
+from collections import OrderedDict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--photo', dest='photo', action="store_true")
@@ -23,36 +16,38 @@ env = Environment(loader=FileSystemLoader('templates'))
 env.globals['SITE_NAME'] = 'CamPL-NG'
 
 if args.photo:
-  IMAGE_SOURCE = "http://loremflickr.com/%s/%s/?user=cambridge%%20university"
+    IMAGE_SOURCE = "http://loremflickr.com/%s/%s/?user=cambridge%%20university"
 else:
-  IMAGE_SOURCE = "http://placehold.it/%sx%s"
+    IMAGE_SOURCE = "http://placehold.it/%sx%s"
+
 
 def main():
-  from site_content.examples import EXAMPLES
-  from site_content.functions import FUNCTIONS
-  from site_content.structure import pages, front_page
+    from site_content.examples import EXAMPLES
+    from site_content.functions import FUNCTIONS
+    from site_content.structure import pages, front_page
 
-  with open('themes.json') as f:
-    env.globals['COLOURS'] = json.loads(f.read(), object_pairs_hook=OrderedDict)['themes']
+    with open('themes.json') as f:
+        env.globals['COLOURS'] = json.loads(f.read(), object_pairs_hook=OrderedDict)['themes']
 
-  with open('images.json') as f:
-    env.globals['IMAGE_STYLES'] = json.loads(f.read(), object_pairs_hook=OrderedDict)['images']
-    
-  # add functions
-  for f in FUNCTIONS:
-    env.globals[f.func_name] = f
+    with open('images.json') as f:
+        env.globals['IMAGE_STYLES'] = json.loads(f.read(), object_pairs_hook=OrderedDict)['images']
 
-  with open('site_content/links.json') as f:
-    env.globals['LINKS'] = json.loads(f.read(), object_pairs_hook=OrderedDict)
-    
-  env.globals['EXAMPLES'] = EXAMPLES
+    # add functions
+    for f in FUNCTIONS:
+        env.globals[f.__name__] = f
 
-  env.globals['MENU'] = pages
+    with open('site_content/links.json') as f:
+        env.globals['LINKS'] = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
-  for page in pages:
-    page.render(env)
+    env.globals['EXAMPLES'] = EXAMPLES
 
-  front_page.render(env)
+    env.globals['MENU'] = pages
 
-if __name__== "__main__":
-  main()
+    for page in pages:
+        page.render(env)
+
+    front_page.render(env)
+
+
+if __name__ == "__main__":
+    main()
--- site_content/examples.py
+++ site_content/examples.py
@@ -1,129 +1,131 @@
-from .functions import lipsum, random_image, random_article
-from random import random
 import string
 
+from random import random
+
+from .functions import lipsum, random_image, random_article
+
 NAV_ITEMS = [
-  {
-    'title': 'Active',
-    'id': 'tab1',
-    'disabled': False,
-    'active': True,
-    'content': lipsum(2),
-  },
-  {
-    'title': 'Link',
-    'id': 'tab2',
-    'disabled': False,
-    'active': False,
-    'content': lipsum(2),
-  },
-  {
-    'title': 'Another Link',
-    'id': 'tab3',
-    'disabled': False,
-    'active': False,
-    'content': lipsum(2),
-  },
-  {
-    'title': 'Disabled',
-    'disabled': True
-  }
+    {
+        'title': 'Active',
+        'id': 'tab1',
+        'disabled': False,
+        'active': True,
+        'content': lipsum(2),
+    },
+    {
+        'title': 'Link',
+        'id': 'tab2',
+        'disabled': False,
+        'active': False,
+        'content': lipsum(2),
+    },
+    {
+        'title': 'Another Link',
+        'id': 'tab3',
+        'disabled': False,
+        'active': False,
+        'content': lipsum(2),
+    },
+    {
+        'title': 'Disabled',
+        'disabled': True
+    }
 ]
 
 LOCAL_FOOTER_LINKS = [
-  [
-    {
-      "header": "About the Faculty",
-      "links": [
-        "Lorem ipsum dolor",
-        "Sit amet",
-        "Consectetur adipisicing elit",
-        "Sed do eiusmod",
-        "Tempor Incididunt"
-      ]
-    },
-    {
-      "header": "Research",
-      "links": [
-        "Ut labore et dolore",
-        "Ut enim ad minim veniam",
-        "Magna aliqua"
-      ]
-    }
-  ],
-  [
-    {
-      "header": "About the University",
-      "links": [
-        "Quis nostrud exercitation",
-        "Ullamco laboris nisi",
-        "Ut aliquip ex ea commodo",
-        "Duis aute irure dolor"
-      ]
-    },
-    {
-      "header": "Libraries & facilities",
-      "links": [
-        "In reprehenderit in voluptate",
-        "Velit esse cillum dolore",
-        "Eu fugiat nulla pariatur",
-        "Duis aute irure dolor"
-      ]
-    }
-  ],
-  [
-    {
-      "header": "Graduate",
-      "links": [
-        "Excepteur sint occaecat",
-        "Cupidatat non proident"
-      ]
-    }
-  ],
-  [
-    {
-      "header": "Subjects",
-      "links": [
-        "Sunt in culpa qui officia",
-        "Deserunt mollit anim",
-        "Id est laborum",
-        "Duis aute irure dolor"
-      ]
-    }
-  ]
+    [
+        {
+            "header": "About the Faculty",
+            "links": [
+                "Lorem ipsum dolor",
+                "Sit amet",
+                "Consectetur adipisicing elit",
+                "Sed do eiusmod",
+                "Tempor Incididunt"
+            ]
+        },
+        {
+            "header": "Research",
+            "links": [
+                "Ut labore et dolore",
+                "Ut enim ad minim veniam",
+                "Magna aliqua"
+            ]
+        }
+    ],
+    [
+        {
+            "header": "About the University",
+            "links": [
+                "Quis nostrud exercitation",
+                "Ullamco laboris nisi",
+                "Ut aliquip ex ea commodo",
+                "Duis aute irure dolor"
+            ]
+        },
+        {
+            "header": "Libraries & facilities",
+            "links": [
+                "In reprehenderit in voluptate",
+                "Velit esse cillum dolore",
+                "Eu fugiat nulla pariatur",
+                "Duis aute irure dolor"
+            ]
+        }
+    ],
+    [
+        {
+            "header": "Graduate",
+            "links": [
+                "Excepteur sint occaecat",
+                "Cupidatat non proident"
+            ]
+        }
+    ],
+    [
+        {
+            "header": "Subjects",
+            "links": [
+                "Sunt in culpa qui officia",
+                "Deserunt mollit anim",
+                "Id est laborum",
+                "Duis aute irure dolor"
+            ]
+        }
+    ]
 ]
 
 A_TO_Z = list(
-  [
-    l, lipsum(5) if random() > 0.2 else None, False
-  ] for l in string.ascii_uppercase
+    [
+        l, lipsum(5) if random() > 0.2 else None, False
+    ] for l in string.ascii_uppercase
 )
 
 A_TO_Z[0][1] = lipsum(5)
 A_TO_Z[0][2] = True
 
 CAROUSEL = [
-  (random_article(), random_image(885, 432)),
-  (random_article(), random_image(885, 432)),
-  (random_article(), random_image(885, 432)),
+    (random_article(), random_image(885, 432)),
+    (random_article(), random_image(885, 432)),
+    (random_article(), random_image(885, 432)),
 ]
 
 CAMPAGE_CAROUSEL = [
-  (random_article(), random_image(785, 428)),
-  (random_article(), random_image(785, 428)),
-  (random_article(), random_image(785, 428)),
+    (random_article(), random_image(785, 428)),
+    (random_article(), random_image(785, 428)),
+    (random_article(), random_image(785, 428)),
 ]
 
 VIDEOS = {
-  "16by9": "https://www.youtube.com/embed/Nz8gqWsSpm8",
-  "4by3": "https://www.youtube.com/embed/8-QNAwUdHUQ",
+    "16by9": "https://www.youtube.com/embed/Nz8gqWsSpm8",
+    "4by3": "https://www.youtube.com/embed/8-QNAwUdHUQ",
 }
 
 EXAMPLES = {
-  'NAV_ITEMS': NAV_ITEMS,
-  'LOCAL_FOOTER_LINKS': LOCAL_FOOTER_LINKS,
-  'A_TO_Z': A_TO_Z,
-  'CAROUSEL': CAROUSEL,
-  'CAMPAGE_CAROUSEL': CAMPAGE_CAROUSEL,
-  'VIDEOS': VIDEOS,
+    'NAV_ITEMS': NAV_ITEMS,
+    'LOCAL_FOOTER_LINKS': LOCAL_FOOTER_LINKS,
+    'A_TO_Z': A_TO_Z,
+    'CAROUSEL': CAROUSEL,
+    'CAMPAGE_CAROUSEL': CAMPAGE_CAROUSEL,
+    'VIDEOS': VIDEOS,
 }
