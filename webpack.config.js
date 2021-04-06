586daceb334fb501b16337fc4d39c1abef5a7e98
Bill Collins <bill@bill.works>
2021-04-06 22:05:49 +0100

    Font awesome

--- webpack.config.js
+++ webpack.config.js
@@ -87,6 +87,18 @@ module.exports = (env, argv = {}) => {
                             }
                         }
                     ]
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: '../fonts',
+                            }
+                        }
+                    ]
                 }
             ]
         },
