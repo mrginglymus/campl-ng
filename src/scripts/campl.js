d77539f34554cbb723ff67aeede52d49f9589021
Bill Collins <bill@bill.works>
2021-04-08 20:17:52 +0100

    Scripts

--- src/scripts/campl.js
+++ src/scripts/campl.js
@@ -1,9 +1,15 @@
 import 'campl-ng/styles/campl.scss';
+import 'hammerjs/hammer.js';
+import 'jquery-hammerjs/jquery.hammer.js';
+
+import './carousel.coffee';
+// import './flexbox.coffee'; todo: modernizr
+import './menu.coffee';
+// import './object_fit.coffee'; todo: modernizr
+import './select_tab.coffee';
 
 //import 'tether/dist/js/tether.js';
 //import 'bootstrap/dist/js/bootstrap.js';
 //import 'moment/moment.js';
 //import 'moment/locale/en-gb.js';
 //import 'eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js'
-//import 'hammerjs/hammer.js';
-//import 'jquery-hammerjs/jquery.hammer.js';
