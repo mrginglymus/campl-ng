119863b10240ea8020130ed21bde47b29b898602
Bill Collins <bill@bill.works>
2021-04-12 21:40:01 +0100

    Global header

--- build.js
+++ build.js
@@ -5,9 +5,10 @@ const path = require('path');
 const fs = require('fs')
 
 const content = require('campl-ng/pages/content')
+const links = require('campl-ng/pages/links');
 
 content.forEach(page => {
     const template = pug.compileFile(path.join('src', 'pages', `${page.source}.pug`));
-    const content = template({page});
+    const content = template({page, links});
     fs.writeFileSync(path.join('build', page.destination), content);
 })
---
+++ src/pages/links.js
@@ -0,0 +1,138 @@
+const quicklinks = {
+    "For staff": "http://www.cam.ac.uk/for-staff",
+    "For current students": "http://www.admin.cam.ac.uk/students/gateway",
+    "For alumni": "http://www.alumni.cam.ac.uk/",
+    "For business": "http://www.cam.ac.uk/for-business",
+    "Colleges & departments": "http://www.cam.ac.uk/colleges-and-departments",
+    "Libraries & facilities": "http://www.cam.ac.uk/libraries-and-facilities",
+    "Museums & collections": "http://www.cam.ac.uk/museums-and-collections",
+    "Email & phone search": "http://www.cam.ac.uk/email-and-phone-search"
+}
+const studyDrawer = {
+    title: "Study at Cambridge",
+    link: "http://www.cam.ac.uk/study-at-cambridge",
+    links: [
+        [
+            {
+                title: "Undergraduate",
+                link: "http://www.undergraduate.study.cam.ac.uk",
+                links: {
+                    "Courses": "http://www.undergraduate.study.cam.ac.uk/courses",
+                    "Applying": "http://www.undergraduate.study.cam.ac.uk/applying",
+                    "Events and open days": "http://www.undergraduate.study.cam.ac.uk/events/cambridge-open-days",
+                    "Fees and finance": "http://www.undergraduate.study.cam.ac.uk/finance",
+                    "Student blogs and videos": "http://www.becambridge.com"
+                }
+            }
+        ],
+        [
+            {
+                title: "Graduate",
+                link: "http://www.graduate.study.cam.ac.uk",
+                links: {
+                    "Why Cambridge": "http://www.graduate.study.cam.ac.uk/why-cambridge/welcome-vice-chancellor",
+                    "Course directory": "http://www.graduate.study.cam.ac.uk/ourses",
+                    "How to apply": "http://www.graduate.study.cam.ac.uk/how-do-i-apply",
+                    "Fees and funding": "http://www.admin.cam.ac.uk/students/studentregistry/fees",
+                    "Frequently asked questions": "http://www.graduate.study.cam.ac.uk/faqs/applicant"
+                }
+            }
+        ],
+        [
+            {
+                title: "International Students",
+                link: "http://www.internationalstudents.cam.ac.uk"
+            },
+            {
+                title: "Continuing education",
+                link: "http://www.ice.cam.ac.uk"
+            },
+            {
+                title: "Executive and professional education",
+                link: "http://www.epe.admin.cam.ac.uk"
+            },
+            {
+                title: "Courses in education",
+                link: "http://www.epe.admin.cam.ac.uk"
+            }
+        ]
+    ]
+}
+const aboutDrawer = {
+    title: "About the University",
+    link: "http://www.cam.ac.uk/about-the-university",
+    links: [
+        [
+            {
+                title: "How the University and Colleges work",
+                link: "http://www.cam.ac.uk/about-the-university/how-the-university-and-colleges-work"
+            },
+            {
+                title: "History",
+                link: "http://www.cam.ac.uk/about-the-university/history"
+            },
+            {
+                title: "Visiting the University",
+                link: "http://www.cam.ac.uk/about-the-university/visiting-the-university"
+            },
+            {
+                title: "Term dates and calendars",
+                link: "http://www.cam.ac.uk/about-the-university/term-dates-and-calendars"
+            },
+            {
+                title: "Map",
+                link: "http://map.cam.ac.uk"
+            }
+        ],
+        [
+            {
+                title: "Media relations",
+                link: "http://www.cam.ac.uk/media-relations"
+            },
+            {
+                title: "Video and audio",
+                link: "http://www.cam.ac.uk/video-and-audio"
+            },
+            {
+                title: "Find an expert",
+                link: "http://webservices.admin.cam.ac.uk/fae/"
+            },
+            {
+                title: "Publications",
+                link: "http://www.cam.ac.uk/about-the-university/publications"
+            },
+            {
+                title: "Global Cambridge",
+                link: "http://www.cam.ac.uk/global-cambridge"
+            }
+        ],
+        [
+            {
+                title: "News",
+                link: "http://www.cam.ac.uk/news"
+            },
+            {
+                title: "Events",
+                link: "http://www.admin.cam.ac.uk/whatson"
+            },
+            {
+                title: "Public engagement",
+                link: "http://www.cam.ac.uk/public-engagement"
+            },
+            {
+                title: "Jobs",
+                link: "http://www.jobs.cam.ac.uk"
+            },
+            {
+                title: "Give to Cambridge",
+                link: "https://www.philanthropy.cam.ac.uk"
+            }
+        ]
+    ]
+}
+
+module.exports = {
+    quicklinks,
+    studyDrawer,
+    aboutDrawer
+}
