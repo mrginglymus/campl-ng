1baae3eb412cd02e70b2b264035bc37e71c187ee
Bill Collins <bill@bill.works>
2021-04-12 13:52:29 +0100

    Burn hammer, carousel swipe

--- src/scripts/campl.js
+++ src/scripts/campl.js
@@ -1,8 +1,5 @@
 import 'campl-ng/styles/campl.scss';
-import 'hammerjs/hammer.js';
-import 'jquery-hammerjs/jquery.hammer.js';
 
-import './carousel.coffee';
 // import './flexbox.coffee'; todo: modernizr
 import './menu';
 // import './object_fit.coffee'; todo: modernizr
